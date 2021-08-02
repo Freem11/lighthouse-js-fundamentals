f428ac342ae07341a183c64bb0f3b969993d5b74
Freem11 <freem1985@gmail.com>
2021-08-02 12:37:29 -0700

    functions work

---
+++ conditionals-execution.js
@@ -0,0 +1,15 @@
+
+
+
+const whichSchool  = function (age) {
+  if(age < 13) {
+    console.log("Elementary School");
+  }else if(age >= 13 && age <= 18) {
+    console.log("Secondary School");
+  }else{
+    console.log("Lighthouse Labs");
+  }
+};
+whichSchool(40)
+
+
