652d365fb01eceb13565a0b218d712a350bf1306
Freem11 <freem1985@gmail.com>
2021-08-04 13:42:40 -0700

    Aug 4 updates

--- packing-list.js
+++ packing-list.js
@@ -2,7 +2,7 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("Kitchen stuff to pack:");
 let i = 0; 
-while(i < packingList.length;) {
+while(i < packingList.length) {
   console.log(packingList[i]);
   i++
 }
